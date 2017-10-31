8a93a05476c395955e7ae374576776670c3aff37
ybinstock <ybinstock@gmail.com>
2017-10-31 12:04:36 +0100

    first

---
+++ app.js
@@ -0,0 +1,38 @@
+function startScript() {
+  var entity = document.querySelector('.movie');
+
+  document.querySelector('a-assets').addEventListener('loaded', function() {
+    console.log("OK LOADED");
+  });
+
+  window.onload = startScript;
+
+//For video
+//  function showVideo() {
+//    $(entity).attr('visible', 'true');
+//    $(entity).attr('autoplay', 'true');
+//    console.log('entity', entity)
+//  }
+//
+//  setTimeout(showVideo, 6000);
+//
+//  entity.addEventListener('materialvideoloadeddata', function(event) {
+//    console.log('materialvideoloadeddata');
+//    entity.play();
+//  });
+
+}
+
+//
+//$(document).ready(function() {
+//  var entity = document.querySelector('.movie');
+//
+////For video
+//  function showVideo() {
+//    entity.play();
+//    console.log('hello')
+//  }
+//
+//  setTimeout(showVideo, 4000);
+//
+//});
